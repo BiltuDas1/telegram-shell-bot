640b30a1d2f6ed6f0f70e31460ea2959b7bb5095
BiltuDas1 <billionto@gmail.com>
2022-11-22 12:09:13 +0530

    Revert for Fixing bugs

--- urlscan.sh
+++ urlscan.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-source bot.conf
+id=$(sed -n '/### Target Group/,/#$$#/p' 'temp/config_mod.ini' | sed '/#/d')
+frdid=$(sed -n '/### Forward Id/,/#$$#/p' 'temp/config_mod.ini' | sed '/#/d')
 url="$1"
-fetch="https://api.telegram.org/bot${BOTTOKEN}"
+apikey=$(sed -n '/### VT API key/,/#$$#/p' 'temp/config_mod.ini' | sed '/#/d')
+token=$(sed -n '/### Bot Token/,/#$$#/p' 'temp/config_mod.ini' | sed '/#/d')
+fetch="https://api.telegram.org/bot$token"
 urlmsgid="$2"
 # Starting
-curl --Silent "$fetch/sendMessage?chat_id=${TARGETGROUP}&text=Scanning The Url🔍" --Output "${WORKINGDIR}/temp/bot.log"
-scan=$(curl -X POST http://www.virustotal.com/vtapi/v2/url/report -F apikey=${VTKEY} -F resource=$url)
+curl --Silent "$fetch/sendMessage?chat_id=$id&text=Scanning The Url🔍" --Output "temp/bot.log"
+scan=$(curl -X POST http://www.virustotal.com/vtapi/v2/url/report -F apikey=$apikey -F resource=$url)
 rescode=$(echo $scan | jq '.response_code')
 if [ "$rescode" == "1" ]; then
   # If URL found into database
@@ -13,33 +16,13 @@ if [ "$rescode" == "1" ]; then
   total=$(echo $scan | jq '.total')
   positives=$(echo $scan | jq '.positives')
   ## Delete Scan message
-  msg_id=$(jq '.result.message_id' "${WORKINGDIR}/temp/bot.log")
-  curl --Silent "$fetch/deleteMessage?chat_id=${TARGETGROUP}&message_id=$msg_id"
+  msg_id=$(jq '.result.message_id' "temp/bot.log")
+  curl --Silent "$fetch/deleteMessage?chat_id=$id&message_id=$msg_id"
   ## Result
   if [ "$positives" -le "3" ]; then
-<<<<<<< HEAD
-    curl --Silent "$fetch/sendMessage?chat_id=${TARGETGROUP}&text=The URL is safe.☑️" --Output "${WORKINGDIR}/temp/bot.log"
-    msg_id=$(jq '.result.message_id' "${WORKINGDIR}/temp/bot.log")
-    sleep 5
-    curl --Silent "$fetch/deleteMessage?chat_id=${TARGETGROUP}&message_id=$msg_id"
-  else
-    if [ "$positives" -le "5" ]; then
-      curl --Silent "$fetch/forwardMessage?chat_id=${FORWARDID}&from_chat_id=${TARGETGROUP}&message_id=$urlmsgid"
-      curl --Silent "$fetch/deleteMessage?chat_id=${TARGETGROUP}&message_id=$urlmsgid"
-      curl --Silent "$fetch/sendMessage?chat_id=${TARGETGROUP}&text=This Message needs additional Review.📝" --Output "${WORKINGDIR}/temp/bot.log"
-      msg_id=$(jq '.result.message_id' "${WORKINGDIR}/temp/bot.log")
-      sleep 10
-      curl --Silent "$fetch/deleteMessage?chat_id=${TARGETGROUP}&message_id=$msg_id"
-    else
-      curl --Silent "$fetch/deleteMessage?chat_id=${TARGETGROUP}&message_id=$urlmsgid"
-      curl --Silent "$fetch/sendMessage?chat_id=${TARGETGROUP}&text=The URL is Dangerous.⚠️" --Output "${WORKINGDIR}/temp/bot.log"
-      msg_id=$(jq '.result.message_id' "${WORKINGDIR}/temp/bot.log")
-      sleep 5
-      curl --Silent "$fetch/deleteMessage?chat_id=${TARGETGROUP}&message_id=$msg_id"
-=======
     curl --Silent "$fetch/sendMessage?chat_id=$id&text=The URL is safe.☑️" --Output "temp/bot.log"
     msg_id=$(jq '.result.message_id' "temp/bot.log")
-    sleep 3
+    sleep 5
     curl --Silent "$fetch/deleteMessage?chat_id=$id&message_id=$msg_id"
   else
     if [ "$positives" -le "5" ]; then
@@ -47,56 +30,46 @@ if [ "$rescode" == "1" ]; then
       curl --Silent "$fetch/deleteMessage?chat_id=$id&message_id=$urlmsgid"
       curl --Silent "$fetch/sendMessage?chat_id=$id&text=This Message needs additional Review.📝" --Output "temp/bot.log"
       msg_id=$(jq '.result.message_id' "temp/bot.log")
-      sleep 3
+      sleep 10
       curl --Silent "$fetch/deleteMessage?chat_id=$id&message_id=$msg_id"
     else
       curl --Silent "$fetch/deleteMessage?chat_id=$id&message_id=$urlmsgid"
       curl --Silent "$fetch/sendMessage?chat_id=$id&text=The URL is Dangerous.⚠️" --Output "temp/bot.log"
       msg_id=$(jq '.result.message_id' "temp/bot.log")
-      sleep 3
+      sleep 5
       curl --Silent "$fetch/deleteMessage?chat_id=$id&message_id=$msg_id"
->>>>>>> parent of f86cca2... Updated Timings of Deleting Command Output
     fi
   fi
 else
   # if url not found into database
-  curl -X POST http://www.virustotal.com/vtapi/v2/url/report -F apikey=${VTKEY} -F url=$url
-  scan=$(curl -X POST http://www.virustotal.com/vtapi/v2/url/report -F apikey=${VTKEY} -F resource=$url)
+  curl -X POST http://www.virustotal.com/vtapi/v2/url/report -F apikey=$apikey -F url=$url
+  scan=$(curl -X POST http://www.virustotal.com/vtapi/v2/url/report -F apikey=$apikey -F resource=$url)
   ## Collecting Information
   total=$(echo $scan | jq '.total')
   positives=$(echo $scan | jq '.positives')
   ## Delete Scan message
-  msg_id=$(jq '.result.message_id' "${WORKINGDIR}/temp/bot.log")
-  curl --Silent "$fetch/deleteMessage?chat_id=${TARGETGROUP}&message_id=$msg_id"
+  msg_id=$(jq '.result.message_id' "temp/bot.log")
+  curl --Silent "$fetch/deleteMessage?chat_id=$id&message_id=$msg_id"
   ## Result
   if [ "$positives" -le "3" ]; then
-    curl --Silent "$fetch/sendMessage?chat_id=${TARGETGROUP}&text=The URL is safe.☑️" --Output "${WORKINGDIR}/temp/bot.log"
-    msg_id=$(jq '.result.message_id' "${WORKINGDIR}/temp/bot.log")
+    curl --Silent "$fetch/sendMessage?chat_id=$id&text=The URL is safe.☑️" --Output "temp/bot.log"
+    msg_id=$(jq '.result.message_id' "temp/bot.log")
     sleep 5
-    curl --Silent "$fetch/deleteMessage?chat_id=${TARGETGROUP}&message_id=$msg_id"
+    curl --Silent "$fetch/deleteMessage?chat_id=$id&message_id=$msg_id"
   else
     if [ "$positives" -le "5" ]; then
-<<<<<<< HEAD
-      curl --Silent "$fetch/forwardMessage?chat_id=${FORWARDID}&from_chat_id=${TARGETGROUP}&message_id=$urlmsgid"
-      curl --Silent "$fetch/deleteMessage?chat_id=${TARGETGROUP}&message_id=$urlmsgid"
-      curl --Silent "$fetch/sendMessage?chat_id=${TARGETGROUP}&text=This Message needs additional Review.📝" --Output "${WORKINGDIR}/temp/bot.log"
-      msg_id=$(jq '.result.message_id' "${WORKINGDIR}/temp/bot.log")
-      sleep 10
-      curl --Silent "$fetch/deleteMessage?chat_id=${TARGETGROUP}&message_id=$msg_id"
-=======
       curl --Silent "$fetch/forwardMessage?chat_id=$frdid&from_chat_id=$id&message_id=$urlmsgid"
       curl --Silent "$fetch/deleteMessage?chat_id=$id&message_id=$urlmsgid"
       curl --Silent "$fetch/sendMessage?chat_id=$id&text=This Message needs additional Review.📝" --Output "temp/bot.log"
       msg_id=$(jq '.result.message_id' "temp/bot.log")
+      sleep 10
+      curl --Silent "$fetch/deleteMessage?chat_id=$id&message_id=$msg_id"
+    else
+      curl --Silent "$fetch/deleteMessage?chat_id=$id&message_id=$urlmsgid"
+      curl --Silent "$fetch/sendMessage?chat_id=$id&text=The URL is Dangerous.⚠️" --Output "temp/bot.log"
+      msg_id=$(jq '.result.message_id' "temp/bot.log")
       sleep 5
       curl --Silent "$fetch/deleteMessage?chat_id=$id&message_id=$msg_id"
->>>>>>> parent of f86cca2... Updated Timings of Deleting Command Output
-    else
-      curl --Silent "$fetch/deleteMessage?chat_id=${TARGETGROUP}&message_id=$urlmsgid"
-      curl --Silent "$fetch/sendMessage?chat_id=${TARGETGROUP}&text=The URL is Dangerous.⚠️" --Output "${WORKINGDIR}/temp/bot.log"
-      msg_id=$(jq '.result.message_id' "${WORKINGDIR}/temp/bot.log")
-      sleep 5
-      curl --Silent "$fetch/deleteMessage?chat_id=${TARGETGROUP}&message_id=$msg_id"
     fi
   fi
 fi
